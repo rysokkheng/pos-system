2bac67cf3c7aa99d2fa7dfda87b7ad89d13c76cc
rysokkheng <sokkhengry168@gmail.com>
2020-09-28 16:38:48 +0700

    fix role permission

--- resources/views/admin/role-create.blade.php
+++ resources/views/admin/role-create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-  
+
 
 <div class="row wrapper border-bottom white-bg page-heading">
     <div class="col-lg-10">
@@ -33,35 +33,58 @@
 @endif
 
 
-<div class="wrapper wrapper-content animated fadeInRight">       
+<div class="wrapper wrapper-content animated fadeInRight">
    <div class="row">
         <div class="col-sm-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-content">
                     <div class="form-horizontal">
                   {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
-                    
-                       
+
+
                         <div class="form-group">
                             <label class="col-sm-1 control-label">RolesName</label>
                             <div class="col-sm-5">
                                 {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control','autocomplete'=>'off')) !!}
                             </div>
-                           
+
                         </div>
                         <div class="col-sm-12">
                           <div class="form-group">
-                            <div class="col-sm-">
+                            <div class="col-sm-2">
                                 <strong>Roles:</strong><p></p>
                                 <p>
                                 @foreach($permission as $value)
-                                     <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
-                                    {{ $value->name }}</label>
-                                <p/>
+                                    @if ($value->groupid == 1)
+                                       <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
+                                       {{ $value->name }}</label>
+                                    @endif
+                                    <p/>
                                 @endforeach
                             </div>
-                             
-                            
+                              <div class="col-sm-2">
+                                  <strong>User:</strong><p></p>
+                                  <p>
+                                  @foreach($permission as $value)
+                                    @if ($value->groupid == 2)
+                                       <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
+                                        {{ $value->name }}</label>
+                                    @endif
+                                  <p/>
+                                  @endforeach
+                              </div>
+                              <div class="col-sm-2">
+                                  <strong>Products:</strong><p></p>
+                                  <p>
+                                  @foreach($permission as $value)
+                                     @if ($value->groupid == 3)
+                                       <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'name')) }}
+                                       {{ $value->name }}</label>
+                                      @endif
+                                  <p/>
+                                  @endforeach
+                              </div>
+
                           </div>
                         </div>
 
@@ -79,6 +102,6 @@
     </div>
 </div>
 
-  
+
 
 @endsection
